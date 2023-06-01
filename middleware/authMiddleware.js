93b86f3553f4d40a28701f7f50971d624d58a06b
walid hamdi <walidhamdidev@gmail.com>
2023-06-01 13:40:53 +0100

    Implement HTTP-only cookie login and register

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const authenticate = (req, res, next) => {
   try {
     const header = req.headers.authorization;
-    const token = header.split(" ")[1];
+    const token = header?.split(" ")[1];
 
     if (!token) {
       return res.status(401).json({ error: "Authorization token is missing" });
