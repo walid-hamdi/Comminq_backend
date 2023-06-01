53060409ffdfd1c3e9082ee921104679fdca4760
walid hamdi <walidhamdidev@gmail.com>
2023-06-01 22:46:40 +0100

    Allow cookieParser

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 
 import config from "./startups/config.js";
+import cookieParser from "cookie-parser";
 import db from "./startups/db.js";
 import routes from "./startups/routes.js";
 import storage from "./startups/storage.js";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 config();
 
 app.use(
